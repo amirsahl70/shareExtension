97debc68ba0fd371c05446b8eaa473537966d3c5
Amir <amir@Amirs-Mac.local>
2018-07-27 23:05:03 -0700

    Initial Commit

---
+++ shareExtensionApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shareExtensionApp
+//
+//  Created by Amir on 7/27/2016.
+//  Copyright © 2018 com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
